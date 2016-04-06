93ef5c2b9ebd8689be4e4651cc8f81d25cf799c2
Mickey <develop.agility@gmail.com>
2016-04-06 17:30:45 -0600

    refactored knex stuff

--- routes/users.js
+++ routes/users.js
@@ -4,25 +4,25 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
 const bcrypt = require('bcrypt');
-const dt = require('../db/tables');
+const Users = function() { return knex('users') };
 
 router.get('/', function(req, res, next) {
-  dt.Users().then(function(users){
+  Users().then(function(users){
     res.json(users);
   });
 });
 
-router.post('/signup', valid.Signup, (req, res, next) =>{
-  let salt = bcrypt.genSaltSync(10);
-  let hash = bcrypt.hashSync(req.body.password, salt);
-  dt.Users()
-    .insert({
-      name: req.body.name,
-      email: req.body.email,
-      password: hash
-    })
-    .then(() => {})
-    .catch(err =>{ next(new Error(err)) });
-});
+// router.post('/signup', valid.Signup, (req, res, next) =>{
+//   let salt = bcrypt.genSaltSync(10);
+//   let hash = bcrypt.hashSync(req.body.password, salt);
+//   Users()
+//     .insert({
+//       name: req.body.name,
+//       email: req.body.email,
+//       password: hash
+//     })
+//     .then(() => {})
+//     .catch(err =>{ next(new Error(err)) });
+// });
 
 module.exports = router;
